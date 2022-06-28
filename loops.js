7659eb153dfc3e21410bc9b9013306347e63e731
carladomini <carr19800@gmail.com>
2022-06-27 22:39:40 -0300

    first commit

---
+++ loops.js
@@ -0,0 +1,16 @@
+var boton = document.getElementById('boton');
+var lista = document.getElementById('lista');
+var checks = document.querySelectorAll('.valores');
+
+boton.addEventListener ('click',function(){
+    lista.innerHTML = '';
+    checks.forEach((e)=>{
+      if(e.checked == true){
+         var elemento = document.createElement('li');
+         elemento.className = 'list-group-item';
+         elemento.innerHTML = e.value;
+         lista.appendChild(elemento);
+      }
+    });
+});
+
